91ad2031378479350f8b73040db3d7768a19c08a
Afrizal Mufriz Fouji <63228544+aafrzl@users.noreply.github.com>
2024-01-30 22:09:55 +0700

    first commit ✨

---
+++ README.md
@@ -0,0 +1,23 @@
+## ✨ Website ucapan ulang tahun
+
+[demo](https://hbd-jubed.netlify.app/)
+
+## 🚀 Quick start
+
+1. **Clone Repo**
+
+   ```bash
+   # clone repo dengan git command berikut
+   $ git clone https://github.com/aafrzl/ucapan-ultah.git
+
+   # masuk ke folder project
+   $ cd ucapan-ultah
+
+   # Buka di text editor kesayangan
+   $ code .
+   ```
+
+2. **Buka dengan Live Server**
+
+3. **Deploy ke Netlify**
+   Deploy dengan Netlify drag and drop folder project ke Netlify [Netlify](https://www.netlify.com/)
---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+// login page scripts
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  validateForm();
+});
+
+function validateForm() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  //validasi username dan password, username dan password bisa diganti sesuai keinginan
+  if (username === "user" && password === "user") {
+    Swal.fire({
+      icon: "success",
+      title: "Login berhasil!",
+      text: "Selamat datang ayanggg",
+      showConfirmButton: false,
+      timer: 1500,
+    }).then(function () {
+      window.location.href = "birthday.html";
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Login gagal!",
+      text: "Coba cek lagi username sama passwordnya ya:)",
+      confirmButtonText: "Coba lagi",
+      confirmButtonColor: "#ff7675",
+    });
+  }
+}
